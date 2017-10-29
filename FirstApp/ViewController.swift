ea2784861d70ab6677d40cb3bb84136fa0555411
薛宇宏 <jack40111@icloud.com>
2017-10-29 23:21:33 +0800

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by 薛宇宏 on 2017/10/29.
+//  Copyright © 2017年 薛宇宏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
